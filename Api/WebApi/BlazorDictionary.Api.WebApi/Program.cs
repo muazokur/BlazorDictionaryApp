7cae07906f99594f6a896b0ff062fe0a31c9c8bc
Mustafa Muaz Okur <mustafamuaz-okur@hotmail.com>
2023-03-19 13:02:35 +0300

    created ApplicationRegistiration

--- Api/WebApi/BlazorDictionary.Api.WebApi/Program.cs
+++ Api/WebApi/BlazorDictionary.Api.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using BlazorDictionary.Api.Application.Extensions;
 using BlazorDictionary.Infrastructure.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationRegistiration();
 builder.Services.AddInfrastructureRegistiration(builder.Configuration);
 
 var app = builder.Build();
