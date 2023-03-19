b569311d6a06fb45592c10f56de8d40fe129edbc
Mustafa Muaz Okur <mustafamuaz-okur@hotmail.com>
2023-03-19 14:02:22 +0300

    created UserLogin

--- Api/Core/BlazorDictionary.Api.Application/BlazorDictionary.Api.Application/Extensions/Registiration.cs
+++ Api/Core/BlazorDictionary.Api.Application/BlazorDictionary.Api.Application/Extensions/Registiration.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorDictionary.Api.Application.Extensions
 {
-    internal class Registiration
+    public static class Registiration
     {
+        public static IServiceCollection AddApplicationRegistiration(this IServiceCollection services)
+        {
+            var assm=Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(assm);
+            services.AddAutoMapper(assm);
+            services.AddValidatorsFromAssembly(assm);
+
+            return services;
+        }
     }
 }
---
+++ Api/Core/BlazorDictionary.Api.Application/BlazorDictionary.Api.Application/Features/Commands/User/LoginUserCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using BlazorDictionary.Common.Models.RequestModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDictionary.Api.Application.Features.Commands.User
+{
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+    {
+        public LoginUserCommandValidator()
+        {
+            RuleFor(i => i.EmailAdress).NotNull()
+                .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible)
+                .WithMessage("{PropertyName} not a valid email address");
+
+            RuleFor(i => i.Password).NotNull()
+                .MinimumLength(6).WithMessage("{PropertyName} should at least be {MinLenght} characters");
+        }
+    }
+}
