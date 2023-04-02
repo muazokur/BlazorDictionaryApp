74e537fdaef05d68ef5465acd432291f243213b9
Mustafa Muaz Okur <mustafamuaz-okur@hotmail.com>
2023-04-03 02:41:05 +0300

    created Projections.FavoriteService add FavoriteService and updated Worker.cs

---
+++ Projections/BlazorDictionart.Projections.FavoriteService/Services/FavoriteService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorDictionary.Common.Events.Entry;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDictionary.Projections.FavoriteService.Services
+{
+    public class FavoriteService
+    {
+        private readonly string connectionString;
+
+        public FavoriteService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public async Task CreateEntryFav(CreateEntryFavEvent @event)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            await connection.ExecuteAsync("INSERT INTO EntryFavorite (Id,EntryId,CreatedById,CreateDate) " +
+                "VALUES (@Id,@EntryId,@CreatedById,@CreateDate, GETDATE())", new
+                {
+                    Id = Guid.NewGuid(),
+                    EntryId = @event.EntryId,
+                    CreatedById = @event.CreatedBy,
+                });
+        }
+    }
+}
--- Projections/BlazorDictionart.Projections.FavoriteService/Worker.cs
+++ Projections/BlazorDictionart.Projections.FavoriteService/Worker.cs
@@ -1,21 +1,36 @@
-namespace BlazorDictionart.Projections.FavoriteService
+using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.Entry;
+using BlazorDictionary.Common.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace BlazorDictionary.Projections.FavoriteService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            var connStr = configuration.GetConnectionString("SqlServer");
+
+            var favService = new Services.FavoriteService(connStr);
+
+            QueueFactory.CreateBasicConsumer()
+                 .EnsureExchange(DictionaryConstants.FavExcahangeName)
+                 .EnsureQueue(DictionaryConstants.CreateEntryFavQueueName, DictionaryConstants.FavExcahangeName)
+                 .Receive<CreateEntryFavEvent>(fav =>
+                 {
+                     favService.CreateEntryFav(fav).GetAwaiter().GetResult();
+                     _logger.LogInformation($"Received EntryId {fav.EntryId}");
+                 }).StartConsuming(DictionaryConstants.CreateEntryFavQueueName);
         }
     }
 }
