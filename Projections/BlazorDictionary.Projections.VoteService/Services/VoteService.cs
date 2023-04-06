794b2de032e5719fb9f44734339d3e3d2291dd6b
Mustafa Muaz Okur <mustafamuaz-okur@hotmail.com>
2023-04-06 17:04:09 +0300

    fixed VoteService

---
+++ Projections/BlazorDictionary.Projections.VoteService/Services/VoteService.cs
@@ -0,0 +1,51 @@
+﻿using BlazorDictionary.Common.Events.Entry;
+using BlazorDictionary.Common.ViewModels;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDictionary.Projections.VoteService.Services
+{
+    public class VoteService
+    {
+        private readonly string connectionString;
+
+        public VoteService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public async Task CreateEntryVote(CreateEntryVoteEvent vote)
+        {
+            await DeleteEntryVote(vote.EntryId, vote.CreatedBy);
+
+            using var connection = new SqlConnection(connectionString);
+
+            await connection.ExecuteAsync("INSERT INTO ENTRYVOTE (Id,CreateDate,EntryId,VoteType,CratedById) " +
+                "VALUES (@Id,@CreateDate,@EntryId,@VoteType,@CreatedById)", new
+                {
+                    Id = Guid.NewGuid(),
+                    EntryId = vote.EntryId,
+                    CreatedById = vote.CreatedBy,
+                    VoteType= (int)vote.VoteType,
+                    CreateDate=DateTime.Now
+                });;
+        }
+
+        public async Task DeleteEntryVote(Guid entryId,Guid userId)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            await connection.ExecuteAsync("DELETE FROM EntryVote WHERE EntryId=@EntryId AND CRATEDBYID=@UserId", new
+                {
+                    Id = Guid.NewGuid(),
+                    EntryId = entryId,
+                    UserId = userId
+                });
+        }
+    }
+}
--- Projections/BlazorDictionary.Projections.VoteService/Worker.cs
+++ Projections/BlazorDictionary.Projections.VoteService/Worker.cs
@@ -1,21 +1,38 @@
+using BlazorDictionary.Common.Events.Entry;
+using BlazorDictionary.Common.Infrastructure.Extensions;
+using BlazorDictionary.Common;
+using Microsoft.Extensions.Configuration;
+
 namespace BlazorDictionary.Projections.VoteService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger,IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+           // var connStr = configuration.GetConnectionString("SqlServer");
+            var connStr = "Server=DESKTOP-G7KHF4G\\SQLEXPRESS;Initial Catalog=BlazorDictionaryDB;Integrated security=true";
+            var voteService = new Services.VoteService(connStr);
+
+
+            QueueFactory.CreateBasicConsumer()
+                 .EnsureExchange(DictionaryConstants.VoteExchangeName)
+                 .EnsureQueue(DictionaryConstants.CreateEntryVoteQueueName, DictionaryConstants.VoteExchangeName)
+                 .Receive<CreateEntryVoteEvent>(vote =>
+                 {
+                     voteService.CreateEntryVote(vote).GetAwaiter().GetResult();
+
+                     _logger.LogInformation($"Create Entry Received EntryId: {0}, VoteType: {1} ",vote.EntryId,vote.VoteType);
+
+                 }).StartConsuming(DictionaryConstants.CreateEntryVoteQueueName);
         }
     }
 }
